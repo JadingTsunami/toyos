6bcfe4d971ac99e04f08c1754669b0ead711f8a5
JadingTsunami <JadingTsunami@users.noreply.github.com>
2017-09-01 23:08:57 -0700

    Simple 'HELLO WORLD' print.

--- kmain.c
+++ kmain.c
@@ -2,18 +2,14 @@
 
 void kmain() {
     fb_write_cell(0,'H',FB_GREEN,FB_DARK_GREY);
-    fb_move_cursor(1);
-    fb_write_cell(0,'E',FB_GREEN,FB_WHITE);
-    fb_move_cursor(2);
-    fb_write_cell(0,'L',FB_GREEN,FB_WHITE);
-    fb_move_cursor(3);
-    fb_write_cell(0,'L',FB_GREEN,FB_WHITE);
-    fb_write_cell(2,'O',FB_GREEN,FB_WHITE);
-    fb_move_cursor(160);
-    fb_write_cell(0,'W',FB_BLUE,FB_WHITE);
-    fb_write_cell(2,'O',FB_BLUE,FB_WHITE);
-    fb_write_cell(4,'R',FB_BLUE,FB_WHITE);
-    fb_write_cell(8,'L',FB_BLUE,FB_WHITE);
-    fb_write_cell(10,'D',FB_BLUE,FB_WHITE);
+    fb_write_cell(2,'E',FB_GREEN,FB_WHITE);
+    fb_write_cell(4,'L',FB_GREEN,FB_WHITE);
+    fb_write_cell(6,'L',FB_GREEN,FB_WHITE);
+    fb_write_cell(8,'O',FB_GREEN,FB_WHITE);
+    fb_write_cell(2*80+0,'W',FB_BLUE,FB_WHITE);
+    fb_write_cell(2*80+2,'O',FB_BLUE,FB_WHITE);
+    fb_write_cell(2*80+4,'R',FB_BLUE,FB_WHITE);
+    fb_write_cell(2*80+6,'L',FB_BLUE,FB_WHITE);
+    fb_write_cell(2*80+8,'D',FB_BLUE,FB_WHITE);
     return;
 }
